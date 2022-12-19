074412699589a1cd7a1a6c2999ca3ae0cc6af23d
jangwooooo <s22018@gsm.hs.kr>
2022-12-19 11:15:37 +0900

    setting

---
+++ src/main/java/com/example/jobgsm/domain/user/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.jobgsm.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("user")
+public class UserController {
+}
