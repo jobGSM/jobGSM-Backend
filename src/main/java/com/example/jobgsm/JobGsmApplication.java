c1e392c1ef59280a10550fbc77b45dc3ab9ddf07
Kim gyeong su <sk62628504@gmail.com>
2022-12-19 09:31:15 +0900

    develop

--- src/main/java/com/example/jobgsm/JobGsmApplication.java
+++ src/main/java/com/example/jobgsm/JobGsmApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // setting
+// develop
 @SpringBootApplication
 public class JobGsmApplication {
 
