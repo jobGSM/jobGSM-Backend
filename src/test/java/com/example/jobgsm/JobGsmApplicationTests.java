750990c04ebb249ea70cefdd2371a0dea744ed37
Kim gyeong su <sk62628504@gmail.com>
2022-12-19 09:23:21 +0900

    setting

---
+++ src/test/java/com/example/jobgsm/JobGsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jobgsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobGsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
