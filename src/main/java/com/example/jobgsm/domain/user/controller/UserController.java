d5ee2ead6761a8c95ce543ee6c1d0abe1360566f
jangwooooo <s22018@gsm.hs.kr>
2022-12-19 12:16:39 +0900

    마이페이지 로직 구현

--- src/main/java/com/example/jobgsm/domain/user/controller/UserController.java
+++ src/main/java/com/example/jobgsm/domain/user/controller/UserController.java
@@ -1,11 +1,22 @@
 package com.example.jobgsm.domain.user.controller;
 
+import com.example.jobgsm.domain.user.dto.response.MyPageResponse;
+import com.example.jobgsm.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("user")
+@RequestMapping("/user/{id}")
 public class UserController {
+
+    private final UserService userService;
+
+    @ResponseBody
+    @GetMapping
+    public ResponseEntity<MyPageResponse> myPage(@PathVariable Long id) {
+        MyPageResponse myPageResponse = userService.myPage(id);
+        return ResponseEntity.ok().body(myPageResponse);
+    }
 }
--- src/main/java/com/example/jobgsm/domain/user/service/UserService.java
+++ src/main/java/com/example/jobgsm/domain/user/service/UserService.java
@@ -1,9 +1,25 @@
 package com.example.jobgsm.domain.user.service;
 
+import com.example.jobgsm.domain.user.dto.response.MyPageResponse;
+import com.example.jobgsm.domain.user.entity.User;
+import com.example.jobgsm.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public MyPageResponse myPage(Long id) {
+        User user = userRepository.findById(id).get();
+        return MyPageResponse.builder()
+                .id(user.getId())
+                .userId(user.getUserId())
+                .userPassword(user.getUserPassword())
+                .userName(user.getUserName())
+                .userGrade(user.getUserGrade())
+                .build();
+    }
 }
