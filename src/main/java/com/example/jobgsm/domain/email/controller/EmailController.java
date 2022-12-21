78809cb40a08dd812d04d7ad4390f8deb5abe203
Kim gyeong su <sk62628504@gmail.com>
2022-12-21 23:37:12 +0900

    이메일 인증

---
+++ src/main/java/com/example/jobgsm/domain/email/controller/EmailController.java
@@ -0,0 +1,32 @@
+package com.example.jobgsm.domain.email.controller;
+
+import com.example.jobgsm.domain.email.service.EmailService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@AllArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+    @PostMapping("/emailConfirm")
+    @ApiOperation(value = "회원 가입시 이메인 인증", notes = "기존사용하고 있는 이메일을 통해 인증")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<? extends BaseResponseBody> emailConfirm(
+            @RequestBody @ApiParam(value="이메일정보 정보", required = true) String email) throws Exception {
+
+        String confirm = emailService.sendSimpleMessage(email);
+
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, confirm));
+    }
+
+}
---
+++ src/main/java/com/example/jobgsm/domain/email/service/EmailService.java
@@ -0,0 +1,5 @@
+package com.example.jobgsm.domain.email.service;
+
+public interface EmailService {
+    String sendSimpleMessage(String to)throws Exception;
+}
