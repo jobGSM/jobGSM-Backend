06382ff8a9eb8d9e6b73ddc3231505f41ae685b9
Kim gyeong su <sk62628504@gmail.com>
2022-12-25 13:32:46 +0900

    merge 충돌 에러

--- src/main/java/com/example/jobgsm/JobGsmApplication.java
+++ src/main/java/com/example/jobgsm/JobGsmApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+// setting
+// develop
+// 김경수
+
 @SpringBootApplication
 @EnableAsync
 public class JobGsmApplication {
