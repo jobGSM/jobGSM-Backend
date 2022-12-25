5cd44cab68a9cd059deed68b8769427b4dd2648c
Kim gyeong su <sk62628504@gmail.com>
2022-12-25 13:28:49 +0900

    merge 충돌 오류 해결

--- src/main/java/com/example/jobgsm/JobGsmApplication.java
+++ src/main/java/com/example/jobgsm/JobGsmApplication.java
@@ -2,10 +2,9 @@ package com.example.jobgsm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @EnableAsync
 public class JobGsmApplication {
 
